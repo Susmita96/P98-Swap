98e6e339d4b9bcb20fad6a63572344d0ea8dc452
Saksham2006 <63293715+Saksham2006@users.noreply.github.com>
2021-05-26 17:11:20 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,23 @@
+def swap():
+
+    print('\nWelcome to the file swapper app.')
+    path1 = input('Enter the path of the first file :- ')
+    file1 = open(path1, "r")
+    file1.read()
+
+    path2 = input('Enter the path of the second file :- ')
+    file2 = open(path2, "r")
+    file2.read()
+
+    data1 = (file1)
+    data2 = (file2)
+
+    file1 = open(path1, 'w')
+    file1.write(data2)
+
+    file2 = open(path2, 'w')
+    file2.write(data1)
+
+    print('\nThe data of the files have been swapped successfully')
+
+swap()
